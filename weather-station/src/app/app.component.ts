87073e965facb14bb149dc3f632c23f0aa41e8eb
Jeff Remillard <jdr@remgant.net>
2020-07-13 06:25:13 -0400

    checkpoint for chart testing

--- weather-station/src/app/app.component.ts
+++ weather-station/src/app/app.component.ts
@@ -11,6 +11,49 @@ import {Observable} from "rxjs";
 export class AppComponent {
   title = 'weather-station';
   data: Observable<WeatherUpdate[]>;
+
+  tdata: any = <any> {
+    labels: ['11:00', '11:05', '11:10', '11:15', '11:20', '11:25'],
+    datasets: [{
+      yAxisID: 'A',
+      fill: false,
+      lineTension: 0,
+      label: 'Temperature',
+      data: [28, 29, 32, 33, 30, 27],
+      borderWidth: 3,
+      borderColor: 'rgba(255, 99, 132, 1)',
+    },
+      {
+        yAxisID: 'B',
+        fill: false,
+        lineTension: 0,
+        label: 'Humidity',
+        data: [54, 52, 50, 49, 55, 60],
+        borderWidth: 3,
+        borderColor: 'rgba(54, 162, 235, 1)',
+      }]
+  }
+
+  options = {
+    scales: {
+      yAxes: [{
+        id: 'A',
+        type: 'linear',
+        position: 'left',
+        ticks: {
+          beginAtZero: true
+        }
+      },{
+        id: 'B',
+        type: 'linear',
+        position: 'right',
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+
   constructor(private dataService: DataService) {
   }
   ngOnInit() {
