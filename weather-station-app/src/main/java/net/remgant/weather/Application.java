d91fdc5bd2b8442ebbad87ff803d6992e80212de
Jeff Remillard <jdr@remgant.net>
2021-05-03 06:10:50 -0400

    Use Hikari connection pool. #19

--- weather-station-app/src/main/java/net/remgant/weather/Application.java
+++ weather-station-app/src/main/java/net/remgant/weather/Application.java
@@ -8,13 +8,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.mock.jndi.SimpleNamingContextBuilder;
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "net.remgant.weather.dao")
@@ -27,34 +20,7 @@ public class Application extends SpringBootServletInitializer {
     }
 
     public static void main(String[] args) {
-        initInitialContext();
         SpringApplication.run(Application.class, args);
-    }
-
-    private static void initInitialContext() {
-        @SuppressWarnings("deprecation")
-        SimpleNamingContextBuilder builder = new SimpleNamingContextBuilder();
-        String dbUrl = System.getProperty("db.url");
-        String dbUser = System.getProperty("db.user");
-        String dbPwd = System.getProperty("db.pwd");
-        DataSource dataSource = new DriverManagerDataSource(dbUrl, dbUser, dbPwd);
-        builder.bind("java:/comp/env/jdbc/weather", dataSource);
-        try {
-            builder.activate();
-        } catch (NamingException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        Context initContext;
-        try {
-            initContext = new InitialContext();
-            return (DataSource) initContext.lookup("java:/comp/env/jdbc/weather");
-        } catch (NamingException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     @Bean
