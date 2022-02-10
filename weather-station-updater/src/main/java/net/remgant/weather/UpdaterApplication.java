1909c4d2723cd8e919c3855172b0127557b29cd6
Jeff Remillard <jdr@remgant.net>
2022-02-10 07:44:53 -0500

    Remove database updates.

--- weather-station-updater/src/main/java/net/remgant/weather/UpdaterApplication.java
+++ weather-station-updater/src/main/java/net/remgant/weather/UpdaterApplication.java
@@ -3,7 +3,6 @@ package net.remgant.weather;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.rabbitmq.client.DefaultSaslConfig;
-import net.remgant.weather.dao.WeatherUpdateRepository;
 import net.remgant.weather.gson.InstantAdapter;
 import net.remgant.weather.model.WeatherUpdate;
 import org.slf4j.Logger;
@@ -16,7 +15,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -31,7 +29,6 @@ public class UpdaterApplication extends SpringBootServletInitializer {
 
     final static private Logger log = LoggerFactory.getLogger(UpdaterApplication.class);
     private final Clock clock = Clock.systemUTC();
-    private final WeatherUpdateRepository repository;
     private final RabbitTemplate rabbitTemplate;
     static final String directExchangeName = "weather-direct-exchange";
     final private Gson gson;
@@ -40,8 +37,7 @@ public class UpdaterApplication extends SpringBootServletInitializer {
         SpringApplication.run(UpdaterApplication.class, args);
     }
 
-    public UpdaterApplication(WeatherUpdateRepository repository, RabbitTemplate rabbitTemplate) {
-        this.repository = repository;
+    public UpdaterApplication(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
         CachingConnectionFactory connectionFactory = (CachingConnectionFactory) rabbitTemplate.getConnectionFactory();
         connectionFactory.getRabbitConnectionFactory().setSaslConfig(DefaultSaslConfig.EXTERNAL);
@@ -56,22 +52,12 @@ public class UpdaterApplication extends SpringBootServletInitializer {
         if (data.getTimestamp() == null)
             data.setTimestamp(Instant.now(clock));
         log.info("Got {}", data);
-        repository.save(data);
         rabbitTemplate.convertAndSend(directExchangeName, "weather.updates", gson.toJson(data));
     }
 
     @RequestMapping("/user")
     public Map<String, Object> user(Principal principal) {
         return Map.of("text", String.format("Hello User %s!", principal.getName()));
-    }
-
-
-    //TODO added for testing, remove it since it's too dangerous to have in final product
-    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    @RequestMapping(value = "/deleteAll", method = RequestMethod.GET)
-    @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    void deleteAll() {
-        repository.deleteAll();
     }
 
     @Bean
