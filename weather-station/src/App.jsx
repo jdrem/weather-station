d8f584da751c7072ef0dc0e5474f7865d52b036c
Jeff Remillard <jdr@remgant.net>
2025-07-31 15:14:20 -0400

    Getting basic fetch and page render working. #95

--- weather-station/src/App.jsx
+++ weather-station/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState, useEffect} from "react";
+import axios from 'axios';
+import Table from 'react-bootstrap/Table'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [conditions, setConditions] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        axios.get("/api/data")
+            .then(resp => {
+                console.log(resp);
+                setConditions(resp.data);
+            })
+            .catch(error => console.error(error))
+    });
+
+    return (
+        <>
+            <h2>Current Conditions</h2>
+            <Table striped bordered size="sm">
+                <thead>
+                    <tr>
+                        <th>Date/Time</th>
+                        <th>Temperature</th>
+                        <th>Humidity</th>
+                        <th>Pressure</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {conditions.map(c => (
+                    <tr key={c.id}>
+                        <td>{c.timestamp}</td>
+                        <td>{c.tempF}</td>
+                        <td>{c.humidity}</td>
+                        <td>{c.pressure}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </Table>
+        </>);
 }
 
 export default App
