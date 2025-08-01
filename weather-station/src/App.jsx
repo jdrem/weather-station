3432eedeeabb47636fcf176093c92a796cc412cd
Jeff Remillard <jdr@remgant.net>
2025-08-01 15:24:08 -0400

    Formatting values in table for better readability. #95

--- weather-station/src/App.jsx
+++ weather-station/src/App.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect} from "react";
 import axios from 'axios';
 import Table from 'react-bootstrap/Table'
+import { NumericFormat } from 'react-number-format';
 import './App.css'
 
 function App() {
@@ -30,10 +31,11 @@ function App() {
                 <tbody>
                 {conditions.map(c => (
                     <tr key={c.id}>
-                        <td>{c.timestamp}</td>
-                        <td>{c.tempF}</td>
-                        <td>{c.humidity}</td>
-                        <td>{c.pressure}</td>
+                        <td>{c.timestamp.substring(5,7)}/{c.timestamp.substring(8,10)}/{c.timestamp.substring(0,4)}&nbsp;
+                            {c.timestamp.substring(11,13)}:{c.timestamp.substring(14,16)}:{c.timestamp.substring(17,19)}</td>
+                        <td><NumericFormat displayType="text" value={c.tempF} decimalScale={1} /> </td>
+                        <td><NumericFormat displayType="text" value={c.humidity} decimalScale={1} /> </td>
+                        <td><NumericFormat displayType="text" value={c.pressure} decimalScale={1} /> </td>
                     </tr>
                 ))}
                 </tbody>
