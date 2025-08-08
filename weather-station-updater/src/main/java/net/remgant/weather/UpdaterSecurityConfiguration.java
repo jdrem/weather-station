6e8fa78d4bc9695f0987b0d2c2068de6c181cc6d
Jeff Remillard <jdr@remgant.net>
2025-08-08 07:19:15 -0400

    Use basic auth instead of x509 certs. #104

--- weather-station-updater/src/main/java/net/remgant/weather/UpdaterSecurityConfiguration.java
+++ weather-station-updater/src/main/java/net/remgant/weather/UpdaterSecurityConfiguration.java
@@ -1,49 +1,45 @@
 package net.remgant.weather;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class UpdaterSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Value("${user.names: }")
-    private String userNames;
-
-    @Value("${admin.names: }")
-    private String adminNames;
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
                 .anyRequest().authenticated().and()
-                .x509()
-                .subjectPrincipalRegex("CN=(.*?)(?:,|$)")
-                .userDetailsService(userDetailsService());
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
-        for (String userName : userNames.split(",")) {
-            inMemoryUserDetailsManager.createUser(new User(userName, "",
-                    AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER")));
-        }
-        for (String userName : adminNames.split(",")) {
-            inMemoryUserDetailsManager.createUser(new User(userName, "",
-                    AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER,ROLE_ADMIN")));
-        }
-        return inMemoryUserDetailsManager;
+    public UserDetailsService userDetailService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
